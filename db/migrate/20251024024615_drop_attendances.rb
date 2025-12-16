1d1538f75086f9120ea8f9d887b4258b93a7f170
kensan25 <kenkounotameni410@gmail.com>
2025-12-16 10:37:48 +0900

    delete unnecessary migrate file

--- db/migrate/20251024024615_drop_attendances.rb
+++
@@ -1,5 +0,0 @@
-class DropAttendances < ActiveRecord::Migration[7.2]
-  def change
-    drop_table :attendances
-  end
-end
--- db/migrate/20251024024952_create_testsumples.rb
+++
@@ -1,9 +0,0 @@
-class CreateTestsumples < ActiveRecord::Migration[7.2]
-  def change
-    create_table :testsumples do |t|
-      t.string :status
-
-      t.timestamps
-    end
-  end
-end
